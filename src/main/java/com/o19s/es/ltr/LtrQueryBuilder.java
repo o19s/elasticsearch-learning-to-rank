a45b2c429627daa07aba6c5ce815d0afc1b1c041
Doug Turnbull <softwaredoug@gmail.com>
2016-12-28 21:31:28 -0500

    First legit passing ES test

--- src/main/java/com/o19s/es/ltr/LtrQueryBuilder.java
+++ src/main/java/com/o19s/es/ltr/LtrQueryBuilder.java
@@ -19,19 +19,32 @@
 
 package com.o19s.es.ltr;
 
+import ciir.umass.edu.learning.Ranker;
+import ciir.umass.edu.learning.RankerFactory;
 import org.apache.lucene.search.Query;
+import org.elasticsearch.common.ParsingException;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.index.query.AbstractQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryParseContext;
 import org.elasticsearch.index.query.QueryShardContext;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class LtrQueryBuilder extends AbstractQueryBuilder<LtrQueryBuilder> {
     public static final String NAME = "ltr";
+
+    private Ranker _rankLibModel;
+
+    public LtrQueryBuilder(Ranker rankLibModel) {
+        _rankLibModel = rankLibModel;
+    }
 
     public LtrQueryBuilder() {
     }
@@ -51,14 +64,49 @@ public class LtrQueryBuilder extends AbstractQueryBuilder<LtrQueryBuilder> {
     }
 
     public static LtrQueryBuilder fromXContent(QueryParseContext parseContext) throws IOException {
+        XContentParser parser = parseContext.parser();
+
+        RankerFactory rankerFactory = new RankerFactory();
+        Ranker ranker = null;
+        final List<Optional<QueryBuilder>> features = new ArrayList<>();
+
+
+        String queryName = null;
+
+
+        float boost = AbstractQueryBuilder.DEFAULT_BOOST;
+        String currentFieldName = null;
         XContentParser.Token token = parseContext.parser().nextToken();
+        while ((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
+            if (token == XContentParser.Token.FIELD_NAME) {
+                currentFieldName = parser.currentName();
+            }
+            else if (token == XContentParser.Token.START_ARRAY) {
+                while ((token = parser.nextToken()) != XContentParser.Token.END_ARRAY) {
+                    switch (currentFieldName) {
+                        case "features": {
+                            features.add(parseContext.parseInnerQueryBuilder());
+                        }
+                    }
+                }
+            }
+        }
+
+        if (ranker == null) {
+            throw new ParsingException(parser.getTokenLocation(),
+                    "[ltr] query requires a model, none specified");
+        }
+
         assert token == XContentParser.Token.END_OBJECT;
-        return new LtrQueryBuilder();
+        LtrQueryBuilder rVal = new LtrQueryBuilder();
+        rVal.queryName(queryName);
+        return rVal;
     }
 
     @Override
     protected Query doToQuery(QueryShardContext context) throws IOException {
-        return null;//new DummyQuery(context.isFilter());
+        //return new LtrQuery();
+        return null;
     }
 
     @Override
--- src/test/java/com/o19s/es/ltr/LtrQueryBuilderTest.java
+++ src/test/java/com/o19s/es/ltr/LtrQueryBuilderTest.java
@@ -1,11 +1,14 @@
 package com.o19s.es.ltr;
 
 import org.apache.lucene.search.Query;
+import org.elasticsearch.common.ParseFieldMatcher;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.index.query.AbstractQueryBuilder;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.search.internal.SearchContext;
 import org.elasticsearch.test.AbstractQueryTestCase;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 
@@ -22,6 +25,25 @@ public class LtrQueryBuilderTest extends AbstractQueryTestCase<LtrQueryBuilder> 
     protected Collection<Class<? extends Plugin>> getPlugins() {
         return Collections.singletonList(LtrQueryParserPlugin.class);
     }
+
+    @Test
+    public void testQueryParsing() throws IOException {
+        String ltrQuery =       "{  " +
+                                "   \"ltr\": {" +
+                                "      \"model\": \"\",        " +
+                                "      \"features\": [        " +
+                                "         {\"match\": {         " +
+                                "            \"foo\": \"bar\"     " +
+                                "         }},                   " +
+                                "         {\"match\": {         " +
+                                "            \"baz\": \"sham\"     " +
+                                "         }}                   " +
+                                "      ]                      " +
+                                "   } " +
+                                "}";
+        parseQuery(ltrQuery, ParseFieldMatcher.EMPTY);
+    }
+
 
     @Override
     protected LtrQueryBuilder doCreateTestQueryBuilder() {
