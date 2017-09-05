29a8aa74859e1d4e9e2080953a21ea72d77e9158
Doug Turnbull <softwaredoug@gmail.com>
2017-09-05 15:08:51 -0400

    Change search to use sltr

---
+++ demo/search.py
@@ -0,0 +1,41 @@
+baseQuery = {
+  "query": {
+      "match": {
+          "_all": "test"
+       }
+   },
+  "rescore": {
+      "query": {
+        "rescore_query": {
+            "sltr": {
+                "params": {
+                    "keywords": ""
+                },
+                "model": "",
+            }
+         }
+      }
+   }
+}
+
+def ltrQuery(keywords, modelName):
+    import json
+    baseQuery['rescore']['query']['rescore_query']['sltr']['model'] = model
+    baseQuery['query']['match']['_all'] = keywords
+    baseQuery['rescore']['query']['rescore_query']['sltr']['params']['keywords'] = keywords
+    print("%s" % json.dumps(baseQuery))
+    return baseQuery
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from elasticsearch import Elasticsearch
+    esUrl="http://localhost:9200"
+    es = Elasticsearch(timeout=1000)
+    model = "test_6"
+    if len(argv) > 2:
+        model = argv[2]
+    results = es.search(index='tmdb', doc_type='movie', body=ltrQuery(argv[1], model))
+    for result in results['hits']['hits']:
+             print(result['_source']['title'])
+
