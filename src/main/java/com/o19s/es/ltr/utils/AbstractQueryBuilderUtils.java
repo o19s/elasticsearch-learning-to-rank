25ca1fd2a8ec78e96064e5b5f4904e5600ea6347
Jettro Coenradie <jettro@gridshore.nl>
2019-04-23 09:07:20 -0400

    Remove declareStandardFields method from AbstractQueryBuilderUtils
    As the method is now open to subclasses based on the pull request
    https://github.com/elastic/elasticsearch/issues/27865, we can now
    use the method from the parent class.

--- src/main/java/com/o19s/es/ltr/utils/AbstractQueryBuilderUtils.java
+++ src/main/java/com/o19s/es/ltr/utils/AbstractQueryBuilderUtils.java
@@ -2,28 +2,20 @@ package com.o19s.es.ltr.utils;
 
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.xcontent.AbstractObjectParser;
-import org.elasticsearch.index.query.AbstractQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains a few methods copied from the AbstractQueryBuilder class. These methods are not accessible from sub classes
+ * that do not reside in the same package.
+ */
 public class AbstractQueryBuilderUtils {
 
-    /**
-     * Method copied from the {@link org.elasticsearch.index.query.AbstractQueryBuilder}. Scope was reduced to
-     * package private. But we use it in multiple subclasses.
-     *
-     * An issue is send to elastic to ask for a change to make it available again:
-     * https://github.com/elastic/elasticsearch/issues/27865
-     *
-     * @param parser Instance of a parser declare some default values for fields
-     */
-    public static void declareStandardFields(AbstractObjectParser<? extends QueryBuilder, ?> parser) {
-        parser.declareFloat(QueryBuilder::boost, AbstractQueryBuilder.BOOST_FIELD);
-        parser.declareString(QueryBuilder::queryName, AbstractQueryBuilder.NAME_FIELD);
+    private AbstractQueryBuilderUtils() {
+        // Utility class with static methods only
     }
 
     public static void writeQueries(StreamOutput out, List<? extends QueryBuilder> queries) throws IOException {
